549e0f8894d62d7ca4b7e9071339408c3b767455
GaoChengquan <529390053@qq.com>
2018-04-17 10:28:28 +0800

    init

---
+++ src/main/java/com/situ/boot/BootApplication.java
@@ -0,0 +1,12 @@
+package com.situ.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+}
